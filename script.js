69b848b1e4b63c40d64679785ed0284c96e4c816
John Robinson <johnarobinson133@gmail.com>
2020-10-15 13:38:35 -0600

    added more functions to the js file

--- script.js
+++ script.js
@@ -12,3 +12,34 @@ var auditTime = function (timeEl, timeInt) {
         timeEl.addClass("bg-success");
     }
 };
+
+var saveSchedule = function () {
+    var tempScheduleArr = [];
+    $("#schedule .row").each(function () {
+        var this_row = $(this);
+        var row_time = this_row.data("time");
+        var row_taskEl = this_row.find("div:nth-child(2)");
+        var row_task_text = row_taskEl.text();
+
+        tempScheduleArr.push({
+            [row_time]: row_task_text
+        });
+        auditTime(row_taskEl, row_time);
+    });
+    localStorage.setItem("schedule", JSON.stringify(tempScheduleArr));
+};
+
+var loadSchedule = function () {
+    if (scheduleArr.length === 0) {
+        return;
+    };
+    $.each(scheduleArr, function (index, objectData) {
+        var hour = Object.keys(objectData)[0];
+        var task = Object.values(objectData)[0];
+        var targetRow = $("#schedule").find(`[data-time='${hour}']`);
+        targetRow.find(".hourly-task").text(task);
+    });
+};
+
+
+$("#currentDay").text(m.format("dddd, MMMM Do"));
